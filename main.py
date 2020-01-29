a2eb8c69c09796af16d7544c4890c6c80eb1d8b9
Mosley777 <60199068+Mosley777@users.noreply.github.com>
2020-01-29 15:30:29 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+print "Hello World!"
+print "Hello Again"
+print "i like typing this"
+print "this is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print  'I "said" do not touch this.'
+
+
+#Excersise 2
+print "i could have code like this." #bangbang
+# print "This won't run."
+print "This will run."
+
+#Excersise 3
+print "I will now count my chickens:"
+print "Hens",25+30/6
+print "Roosters",100-25*3%4
+print "Now I will count the eggs."
+print  3 + 2 + 1 - 5 + 4 % 2 - 1/4 + 6
+print "Is it true that 3 + 2 < 5-7?"
+print 3 + 2 < 5-7
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
